c5be8f3c7926b97c4a2480c81e4ddd60202a6dc7
Oleg Solntsev <order2@rambler.ru>
2024-07-18 22:43:04 +0300

    Добавил содержимое README + комментарий к реализации репозитория

--- Services/MemoryEntityRepo.cs
+++ Services/MemoryEntityRepo.cs
@@ -7,7 +7,7 @@ namespace unistream_t2.Services
     // Наша реализация репозитория - в виде хранилища в памяти для тестовых целей этой работы, но в продакшене будем использовать что-то сетевое и распределенное 
     public class MemoryEntityRepo : IEntityRepo
     {
-        private readonly ConcurrentDictionary<Guid, Entity> _dic = new();
+        private readonly ConcurrentDictionary<Guid, Entity> _dic = new(); // Использую ConcurrentDictionary, так как у нас WebApi многопоточный
 
         public Task<Entity> GetAsync(Guid id)
         {
